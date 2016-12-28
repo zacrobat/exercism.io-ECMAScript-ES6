839a38f439de56f072c74f063470280e67c8619b
Zac Cobb <zacrobat55@gmail.com>
2016-12-28 13:23:50 +1100

    First commit

---
+++ hello-world/hello-world.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  // hello(name) {
+  //   return `Hello, ${name || 'World'}!`;
+  // }
+    hello(name = 'World') {
+    return `Hello, ${name}!`
+  }
+}
+
+export default HelloWorld;
